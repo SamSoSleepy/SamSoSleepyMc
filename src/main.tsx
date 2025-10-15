215a3fda4763672955fdb319c4d9270581de821b
SamSoSleepy <127093457+SamSoSleepy@users.noreply.github.com>
2025-10-15 10:56:27 +0000

    text

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@
   import { createRoot } from "react-dom/client";
   import App from "./App.tsx";
   import "./index.css";
+  // Load global theme and component styles (contains .deltarune-bg and CSS variables)
+  import "./styles/globals.css";
 
   createRoot(document.getElementById("root")!).render(<App />);
   
